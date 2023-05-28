519a99ad227922add46db9ed17db6d5fa35290d5
Ryan Reynolds Julyan <ryan@julyan.biz>
2023-05-27 20:48:04 -0700

    Create App Builder for App Factory, Extensions, Plugins

---
+++ app/core/app_builder.py
@@ -0,0 +1,26 @@
+import os
+from typing import Any, Dict
+
+from flask import Flask
+
+from configuration.config import (
+    Config,
+    default_config_factory
+)
+from app.core.app_factory import create_app
+from app.extensions.register_extensions import DEFAULT_EXTENSIONS_DIRECTORY, register_extensions
+from app.plugins.register_plugins import DEFAULT_PLUGINS_DIRECTORY, register_plugins
+
+
+def build_app(config_factory: Dict[str, Config], extensions_directory: str = DEFAULT_EXTENSIONS_DIRECTORY, plugins_directory: str = DEFAULT_PLUGINS_DIRECTORY) -> Any:
+  config_factory = {**default_config_factory, **config_factory}
+  flask_env = os.environ.get("FLASK_ENV", "default")
+
+  app = create_app(config_object=config_factory[flask_env])
+
+  register_extensions(app=app, extensions_directory=extensions_directory)
+
+  register_plugins(app=app, plugins_directory=plugins_directory)
+
+  return app
+
