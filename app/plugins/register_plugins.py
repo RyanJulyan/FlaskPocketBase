014fd09023dcc04470f4547e1f4e149ee9ce5a5b
Ryan Reynolds Julyan <ryan@julyan.biz>
2023-05-27 20:46:56 -0700

    Create Plugin logic based on Extensions

---
+++ app/plugins/register_plugins.py
@@ -0,0 +1,34 @@
+import os
+import importlib
+from typing import Any, List
+
+from app.plugins.get_enabled_plugins import get_enabled_plugins
+
+DEFAULT_PLUGINS_DIRECTORY = 'app/plugins/'
+
+
+def list_folders(directory: str) -> List[str]:
+  return [
+    d for d in os.listdir(directory)
+    if os.path.isdir(os.path.join(directory, d))
+  ]
+
+
+def register_plugins(app: Any, plugins_directory: str = DEFAULT_PLUGINS_DIRECTORY,) -> None:
+
+  @app.before_request
+  def before_request() -> None:
+    enabled_plugins = get_enabled_plugins(
+    )  # Get the list of enabled plugins from your database or config file
+
+    all_plugins = list_folders(plugins_directory)
+
+    for plugin in all_plugins:
+      if plugin in enabled_plugins and plugin not in app.blueprints:
+        # The plugin is enabled and not currently registered, so register it
+        blueprint = importlib.import_module(f'.plugins.{plugin}.views',
+                                            'app').blueprint
+        app.register_blueprint(blueprint)
+      elif plugin not in enabled_plugins and plugin in app.blueprints:
+        # The plugin is disabled and currently registered, so unregister it
+        app.blueprints.pop(plugin)
