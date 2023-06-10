61313db890649a9194a0a92618a87b56c056d41a
Ryan Reynolds Julyan <ryan@julyan.biz>
2023-06-10 18:28:24 +0200

    update config

--- configuration/config.py
+++ configuration/config.py
@@ -37,7 +37,7 @@ class Config(object):
     BASE_DIR = BASE_DIR
 
     ###########################
-    # development environment #
+    # Development environment #
     ###########################
     DEBUG = env("DEBUG", False)
     DEBUG_TB_PROFILER_ENABLED = env("DEBUG_TB_PROFILER_ENABLED", False)
@@ -54,6 +54,21 @@ class Config(object):
     )
     SITE_THEME_COLOR = env("SITE_THEME_COLOR", "#3367D6")
     DEVELOPER_NAME = env("DEVELOPER_NAME", "FlaskPocketBase")
+
+    ####################################
+    # Search Engine Optimization (SEO) #
+    ####################################
+    SEO_SUBJECT = env("SEO_SUBJECT", "Rapid Application Development.")
+    SEO_SUBTITLE = env("SEO_SUBTITLE", "Flask BDA Site.")
+    SEO_SUMMARY = env("SEO_SUMMARY", "This site was created using Flask BDA.")
+    SEO_ABSTRACT = env(
+        "SEO_ABSTRACT", "This site was created using FlaskPocketBase."
+    )
+    SEO_KEYWORDS = env(
+        "SEO_KEYWORDS",
+        "Flask, PWA, Python, Rapid Application Development, RAD, Progressive Web App, Flask BDA.",
+    )
+    SEO_REVISIT_AFTER = env("SEO_REVISIT_AFTER", "7 days")
 
     ###################
     # List Formatting #
@@ -97,10 +112,12 @@ class Config(object):
     ###################################################
     # Manage SQLALCHEMY_ENGINE_OPTIONS like pool size #
     ###################################################
+    # https://docs.sqlalchemy.org/en/13/core/engines.html#sqlalchemy.create_engine.params.pool_pre_ping
+    # https://docs.sqlalchemy.org/en/13/core/engines.html#sqlalchemy.create_engine.params.pool_size
+    # https://docs.sqlalchemy.org/en/13/core/engines.html#sqlalchemy.create_engine.params.pool_recycle
     SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_pre_ping": True,  # https://docs.sqlalchemy.org/en/13/core/engines.html#sqlalchemy.create_engine.params.pool_pre_ping
-        # 'pool_size': 0,  # https://docs.sqlalchemy.org/en/13/core/engines.html#sqlalchemy.create_engine.params.pool_size
-        "pool_recycle": -1,  # https://docs.sqlalchemy.org/en/13/core/engines.html#sqlalchemy.create_engine.params.pool_recycle
+        "pool_pre_ping": env("POOL_PRE_PING", True),
+        "pool_recycle": env("POOL_RECYCLE", -1),
     }
 
     ##############################################
@@ -119,22 +136,118 @@ class Config(object):
 
     SQLALCHEMY_BINDS = {"default": DATABASE_URI}
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = env(
+        "SQLALCHEMY_TRACK_MODIFICATIONS", False
+    )
 
-class ProductionConfig(Config):
-    def __init__(self, **kwargs: Any) -> None:
-        config = {
-            **dotenv_values(
-                ".env.production",
-            ),  # load production environment variables
-            **kwargs,  # load passed in variables
-            **os.environ,  # override loaded values with environment variables
-        }
-        super(Config, self).__init__(**config)
-        self.__dict__.update(config)
+    ########################
+    # Application threads. #
+    ########################
+    THREADS_PER_PAGE = env("THREADS_PER_PAGE", 3)
 
-    DATABASE_URI = env("DATABASE_URI", PROD_DATABASE_URI)
+    ############################
+    # Time for session expirey #
+    ############################
+    TIME_TO_EXPIRE = env("TIME_TO_EXPIRE", 3600)
 
-    SQLALCHEMY_BINDS = {"default": DATABASE_URI}
+    ################################################################
+    # Enable protection agains *Cross-site Request Forgery (CSRF)* #
+    ################################################################
+    CSRF_ENABLED = env("CSRF_ENABLED", True)
+    CSRF_SESSION_KEY = env("CSRF_SESSION_KEY", "secret")
+
+    #############################################
+    # Secret key for signing cookies and Tokens #
+    #############################################
+    SECRET_KEY = env("SECRET_KEY", "secret")
+    SECURITY_PASSWORD_SALT = env("SECURITY_PASSWORD_SALT", "secret")
+
+    ################################################
+    # Secret key for signing JWT (JSON Web Tokens) #
+    ################################################
+    JWT_SECRET_KEY = env("JWT_SECRET_KEY", "secret")
+    JWT_ACCESS_TOKEN_EXPIRES = env("JWT_ACCESS_TOKEN_EXPIRES", 3600)
+    JWT_BLACKLIST_ENABLED = env("JWT_BLACKLIST_ENABLED", True)
+    JWT_BLACKLIST_TOKEN_CHECKS = env(
+        "JWT_BLACKLIST_TOKEN_CHECKS", ["access", "refresh"]
+    )
+
+    ########################
+    # CORS ORIGINS allowed #
+    ########################
+    CORS_ORIGINS = env("CORS_ORIGINS", "*")
+
+    ###############################
+    # Default rate limit for site #
+    ###############################
+    DEFAULT_LIMITS = [
+        # "200 per day",
+        # "100/day",
+        # "500/7days",
+        # "50 per hour",
+        "3/second",
+    ]
+
+    DEFAULT_LIMITS = env("DEFAULT_LIMITS", DEFAULT_LIMITS)
+
+    #####################
+    # Email Credentails #
+    #####################
+    MAIL_SERVER = env("MAIL_SERVER", "smtp.mailtrap.io")
+    MAIL_PORT = env("MAIL_PORT", 2525)
+    MAIL_USERNAME = env("MAIL_USERNAME", "email@example.com")
+    MAIL_PASSWORD = env("MAIL_PASSWORD", "example password")
+    MAIL_USE_TLS = env("MAIL_USE_TLS", True)
+    MAIL_USE_SSL = env("MAIL_USE_SSL", False)
+    DEFAULT_MAIL_SENDER = env(
+        "DEFAULT_MAIL_SENDER", "Flask BDA <me@example.com>"
+    )
+
+    ###############
+    # File Upload #
+    ###############
+    UPLOAD_FOLDER = env("UPLOAD_FOLDER", "app/static/uploads")
+    ALLOWED_EXTENSIONS = env(
+        "ALLOWED_EXTENSIONS",
+        set(
+            [
+                "txt",
+                "rtf",
+                "docx",
+                "doc",
+                "docm",
+                "dotx",
+                "odt",
+                "xlsx",
+                "xlsm",
+                "xlsb",
+                "xls",
+                "xltx",
+                "ods",
+                "csv",
+                "xml",
+                "json",
+                "pdf",
+                "png",
+                "jpg",
+                "jpeg",
+                "gif",
+            ]
+        ),
+    )
+
+    ###########
+    # Swagger #
+    ###########
+    SWAGGER_BLUEPRINT_URL_PREFIX = env(
+        "SWAGGER_BLUEPRINT_URL_PREFIX", "/swagger"
+    )
+    SWAGGER_URL = env(
+        "SWAGGER_URL", "/api/docs"
+    )  # URL for exposing Swagger UI (without trailing '/')
+    SWAGGER_API_URL = env(
+        "SWAGGER_API_URL", "swagger.json"
+    )  # Our API url (can of course be a local resource)
 
 
 class DevelopmentConfig(Config):
@@ -169,6 +282,23 @@ class TestConfig(Config):
 
     TESTING = True
     DATABASE_URI = env("DATABASE_URI", TEST_DATABASE_URI)
+
+    SQLALCHEMY_BINDS = {"default": DATABASE_URI}
+
+
+class ProductionConfig(Config):
+    def __init__(self, **kwargs: Any) -> None:
+        config = {
+            **dotenv_values(
+                ".env.production",
+            ),  # load production environment variables
+            **kwargs,  # load passed in variables
+            **os.environ,  # override loaded values with environment variables
+        }
+        super(Config, self).__init__(**config)
+        self.__dict__.update(config)
+
+    DATABASE_URI = env("DATABASE_URI", PROD_DATABASE_URI)
 
     SQLALCHEMY_BINDS = {"default": DATABASE_URI}
 
