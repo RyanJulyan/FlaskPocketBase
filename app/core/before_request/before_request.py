ea3f642acbb069b29a77feb2c99d3f8965907068
Ryan Reynolds Julyan <ryan@julyan.biz>
2023-06-11 13:47:35 +0200

    Update before_request to switch tenant and create DB + Tables

--- app/core/before_request/before_request.py
+++ app/core/before_request/before_request.py
@@ -1,6 +1,7 @@
 from typing import Any
 
-from flask import g, request
+from flask import g, request, current_app as ca
+from sqlalchemy_utils import database_exists, create_database
 
 
 def before_request(app: Any) -> None:
@@ -19,3 +20,21 @@ def before_request(app: Any) -> None:
         elif len(host) == 3 and host[0] != "www":
             g.organization = host[0]
             app.logger.info("Organisation changed: " + g.organization)
+
+        # Set database to tenant_name
+        ca.db.choose_tenant(g.organization)
+
+        # Create database if it does not exist.
+        if app.config["AUTO_CREATE_DATABASE"]:
+            if not database_exists(ca.db.get_engine().url):
+                create_database(ca.db.get_engine().url)
+            else:
+                # Connect the database if exists.
+                ca.db.get_engine().connect()
+
+        # Build the database:
+        print(app.config["SQLALCHEMY_BINDS"])
+        if app.config["AUTO_CREATE_TABLES_FROM_MODELS"]:
+            # This will create the database tables using SQLAlchemy
+            with app.app_context():
+                ca.db.create_all()
