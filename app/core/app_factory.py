41d6a1cfbdb6197d219d4316cfe82731ea68ee33
Ryan Reynolds Julyan <ryan@julyan.biz>
2023-06-11 18:20:42 +0200

    add template_folder to Flask app constructor

--- app/core/app_factory.py
+++ app/core/app_factory.py
@@ -5,7 +5,7 @@ from flask import Flask
 from configuration.config import Config
 
 
-def create_app(config_object: Config) -> Any:
-    app = Flask(__name__)
+def create_app(config_object: Config, template_folder="templates") -> Any:
+    app = Flask(__name__, template_folder=template_folder)
     app.config.from_object(config_object)
     return app
