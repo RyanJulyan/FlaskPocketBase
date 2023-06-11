9c306e3ed6ffbb47657ad0f0d3c9fa5b5b7a505c
Ryan Reynolds Julyan <ryan@julyan.biz>
2023-06-11 14:44:16 +0200

    Update `errorhandler` to allow for Server Errors and Exceptions to be more generic and catch all!

--- app/core/errorhandler/errorhandler.py
+++ app/core/errorhandler/errorhandler.py
@@ -2,21 +2,33 @@ from typing import Any
 import json
 
 from werkzeug.exceptions import HTTPException
-from flask import g, render_template
+from flask import make_response
 
 
 def errorhandler(app: Any) -> None:
-    @app.errorhandler(HTTPException)
+    @app.errorhandler(Exception)
     def handle_exception(e):
         """Return JSON instead of HTML for HTTP errors."""
+        code = 500
+        name = "Server Error"
+        description = "Internal Server Error"
+        response = make_response()
+        if isinstance(e, HTTPException):
+            code = e.code
+            name = e.name
+            description = e.description
+
+            response = e.get_response()
+        else:
+            name = str(type(e).__name__)
+            description = str(e)
         # start with the correct headers and status code from the error
-        response = e.get_response()
         # replace the body with JSON
         response.data = json.dumps(
             {
-                "code": e.code,
-                "name": e.name,
-                "description": e.description,
+                "code": code,
+                "name": name,
+                "description": description,
             }
         )
         response.content_type = "application/json"
