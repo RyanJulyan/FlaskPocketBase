6c44bd64b255bee931c191c998917a70d478593d
Ryan Reynolds Julyan <ryan@julyan.biz>
2023-06-15 23:38:02 +0200

    create base data model

---
+++ app/models/data/base_model.py
@@ -0,0 +1,15 @@
+from app.core.database.database import db
+
+
+# Define a base model for other database tables to inherit
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(
+        db.DateTime,
+        default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp(),
+    )
+    deleted_at = db.Column(db.DateTime, nullable=True)
