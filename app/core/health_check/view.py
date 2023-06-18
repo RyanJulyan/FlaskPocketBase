e336d37ccfbe9bb1ffcd178532dbf8e23190fd32
Ryan Reynolds Julyan <ryan@julyan.biz>
2023-06-18 17:32:48 +0200

    update health_check to have the status returned too

--- app/core/health_check/view.py
+++ app/core/health_check/view.py
@@ -6,14 +6,17 @@ from flask import g, jsonify, request
 def health_check(app: Any, **kwargs: Any) -> None:
     @app.route("/health_check")
     def health_check() -> None:
-        return jsonify(
-            {
-                "status": 200,
-                "message": "healthy",
-                "organization": g.organization,
-                "site_title": app.config["SITE_TITLE"],
-                "expected_site_url": app.config["SITE_URL"],
-                "actual_site_url": request.environ.get("HTTP_HOST"),
-                **kwargs,
-            }
+        return (
+            jsonify(
+                {
+                    "status": 200,
+                    "message": "healthy",
+                    "organization": g.organization,
+                    "site_title": app.config["SITE_TITLE"],
+                    "expected_site_url": app.config["SITE_URL"],
+                    "actual_site_url": request.environ.get("HTTP_HOST"),
+                    **kwargs,
+                }
+            ),
+            200,
         )
