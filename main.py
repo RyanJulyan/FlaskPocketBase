f3833dbea8fd8f7f31387c88f2893e42558c25f1
RyanJulyan <ryan@julyan.biz>
2023-06-26 05:03:26 +0000

    Update Replit entry point to be main! | Remove Debug Flag on run

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from app.core.app_builder import build_app
 
 app = build_app(health_check_kwargs={"dynamic_health_check_kwargs": "working"})
 
-app.run(host="0.0.0.0", port=5000, debug=True)
+app.run(host="0.0.0.0", port=5000)
