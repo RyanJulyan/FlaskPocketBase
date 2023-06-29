ad86aad3055d0d82f04ab2d92c791d3809cd1199
Ryan Reynolds Julyan <ryan@julyan.biz>
2023-06-29 21:29:38 +0200

    Error Handling response error code

--- app/core/errorhandler/errorhandler.py
+++ app/core/errorhandler/errorhandler.py
@@ -9,19 +9,22 @@ def errorhandler(app: Any) -> None:
     @app.errorhandler(Exception)
     def handle_exception(e):
         """Return JSON instead of HTML for HTTP errors."""
+        response = make_response()
         code = 500
         name = "Server Error"
         description = "Internal Server Error"
-        response = make_response()
+
         if isinstance(e, HTTPException):
+            response = e.get_response()
+
             code = e.code
             name = e.name
             description = e.description
 
-            response = e.get_response()
         else:
             name = str(type(e).__name__)
             description = str(e)
+
         # start with the correct headers and status code from the error
         # replace the body with JSON
         response.data = json.dumps(
@@ -31,5 +34,7 @@ def errorhandler(app: Any) -> None:
                 "description": description,
             }
         )
+
+        response.status = code
         response.content_type = "application/json"
         return response
