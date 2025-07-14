81321a963fbb0161a3a13f1c662a95df5fd3365c
Ryan Julyan <ryan@julyan.biz>
2025-07-14 19:38:26 +0200

    update context_processor

--- app/core/context_processor/context_processor.py
+++ app/core/context_processor/context_processor.py
@@ -2,10 +2,13 @@ from typing import Any
 
 from flask import g
 
+from app.services.util.make_serializable import make_serializable
+
 
 def context_processor(app: Any) -> None:
     @app.context_processor
     def context_processor() -> None:
         return {
-            "organization": g.organization,
+            "organization": getattr(g, "organization", "No Organization Set"),
+            "make_serializable": make_serializable,
         }
